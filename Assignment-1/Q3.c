c606f67317c3a13a0a8bc3b352d0241a2ecda50e
Vansh Nanda <vanshnanda2006@gmail.com>
2025-08-10 16:36:08 +0530

    Create Q3.c

---
+++ Assignment-1/Q3.c
@@ -0,0 +1,13 @@
+// Predict the Output of the following program
+#include<stdio.h>
+
+int main()
+{
+  int i;
+  int arr[5] = {1};
+  for (i = 0; i < 5; i++)
+    printf("%d", arr[i]);
+  return 0;
+}
+
+// Ans 10000
