68575b36954aa51f02f97df7f4b363d5f8780d8f
Vansh Nanda <vanshnanda2006@gmail.com>
2025-09-01 22:48:26 +0530

    Create Q5.cpp

---
+++ Assignment-3/Additional Questions/Q5.cpp
@@ -0,0 +1,6 @@
+// You have an array A of integers of size N, an array B (initially empty) and a stack S (initially
+// empty). You are allowed to do the following operations:
+// a) Take the first element of array A and push it into S and remove it from A.
+// b) Take the top element from stack S, append it to the end of array B and remove it from S.
+// You have to tell if it possible to move all the elements of array A to array B using the above
+// operations such that finally the array B is sorted in ascending order.
