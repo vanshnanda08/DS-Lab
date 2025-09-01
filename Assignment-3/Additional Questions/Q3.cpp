6187702dcd303495545549bd07dcae34e33eddf9
Vansh Nanda <vanshnanda2006@gmail.com>
2025-09-01 22:47:50 +0530

    Create Q3.cpp

---
+++ Assignment-3/Additional Questions/Q3.cpp
@@ -0,0 +1,4 @@
+// Given an array arr[ ] of integers, the task is to find the Next Greater Element for each element of the
+// array in order of their appearance in the array. Note: The Next Greater Element for an element x is
+// the first greater element on the right side of x in the array. Elements for which no greater element
+// exist, consider the next greater element as -1.
