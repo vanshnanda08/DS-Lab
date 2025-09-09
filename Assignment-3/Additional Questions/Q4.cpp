f4c812df5ec061480a41a9441501befd0c431035
Vansh Nanda <vanshnanda2006@gmail.com>
2025-09-09 10:55:25 +0530

    Q4.cpp

--- Assignment-3/Additional Questions/Q4.cpp
+++ Assignment-3/Additional Questions/Q4.cpp
@@ -1,3 +1,51 @@
 // Given an array of integers temperatures represents the daily temperatures, return an
 // array answer such that answer[i] is the number of days you have to wait after the ith day to get a
 // warmer temperature. If there is no future day for which this is possible, keep answer[i] == 0 instead.
+
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+
+#include <iostream>
+#include <stack>
+#include <vector>
+using namespace std;
+
+vector<int> dailyTemperatures(vector<int> &temperatures)
+{
+  int n = temperatures.size();
+  vector<int> ans(n, 0);
+  stack<int> st; // stores indices
+
+  for (int i = 0; i < n; i++)
+  {
+    while (!st.empty() && temperatures[i] > temperatures[st.top()])
+    {
+      int idx = st.top();
+      st.pop();
+      ans[idx] = i - idx;
+    }
+    st.push(i);
+  }
+  return ans;
+}
+
+int main()
+{
+  int n;
+  cout << "Enter number of days: ";
+  cin >> n;
+
+  vector<int> temperatures(n);
+  cout << "Enter temperatures: ";
+  for (int i = 0; i < n; i++)
+    cin >> temperatures[i];
+
+  vector<int> result = dailyTemperatures(temperatures);
+
+  cout << "Answer: ";
+  for (int x : result)
+    cout << x << " ";
+  cout << endl;
+
+  return 0;
+}
