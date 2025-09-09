12df88a16c7c7b16de5cd2e378ee91fb0ac3b9d0
Vansh Nanda <vanshnanda2006@gmail.com>
2025-09-09 10:52:34 +0530

    Update Q1.cpp

--- Assignment-3/Additional Questions/Q1.cpp
+++ Assignment-3/Additional Questions/Q1.cpp
@@ -1,2 +1,51 @@
 // Given an array A, find the nearest smaller element for every element A[i] in the array such that the
 // element has an index smaller than i.
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+
+#include <iostream>
+#include <stack>
+#include <vector>
+using namespace std;
+
+vector<int> nearestSmallerToLeft(vector<int> &A)
+{
+  stack<int> st;
+  vector<int> ans;
+
+  for (int i = 0; i < A.size(); i++)
+  {
+    while (!st.empty() && st.top() >= A[i])
+    {
+      st.pop();
+    }
+    if (st.empty())
+      ans.push_back(-1);
+    else
+      ans.push_back(st.top());
+
+    st.push(A[i]);
+  }
+  return ans;
+}
+
+int main()
+{
+  int n;
+  cout << "Enter size of array: ";
+  cin >> n;
+
+  vector<int> A(n);
+  cout << "Enter elements: ";
+  for (int i = 0; i < n; i++)
+    cin >> A[i];
+
+  vector<int> result = nearestSmallerToLeft(A);
+
+  cout << "Nearest Smaller Elements: ";
+  for (int x : result)
+    cout << x << " ";
+  cout << endl;
+
+  return 0;
+}
